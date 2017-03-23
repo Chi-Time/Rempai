a127be220033d728a93034230ff7295ad224ab0e
James Johnson <music1384@yahoo.co.uk>
2017-03-23 05:02:39 +0000

    Formatted and registered a series of groups.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 // Creates the initial client.
 const Commando = require ("discord.js-commando");
-const client = new Commando.Client();
+const client = new Commando.Client ();
 
 // Setup the client's commands and prefix.
-client.registry.registerDefaults();
-client.registry.registerCommandsIn(__dirname + "/Commands");
+client.registry.registerDefaults ();
+client.registry.registerGroups ([
+    ["bot", "Bot"],
+    ["admin", "Admin"],
+    ["random", "Random"],
+    ["decision", "Decision"],
+]);
+client.registry.registerCommandsIn (__dirname + "/Commands");
 
 client.commandPrefix = "~";
 
 // Setup the client's initial state.
-client.on("ready", () => {
-    client.user.setGame("with ♡Chihiro♡");
-    client.user.setStatus("online");
-    console.log("Logged In");
-})
+client.on ("ready", () => {
+    client.user.setGame ("with ♡Chihiro♡");
+    client.user.setStatus ("online");
+    console.log ("Logged In");
+});
 
 // Log the client into Discord.
-client.login("Mjk0MTUwMDMxMzQxMTI1NjQy.C7Q8FA.jJuIbC9ksIYOWkvfbAXW6s-jazE");
+client.login ("Mjk0MTUwMDMxMzQxMTI1NjQy.C7Q8FA.jJuIbC9ksIYOWkvfbAXW6s-jazE");

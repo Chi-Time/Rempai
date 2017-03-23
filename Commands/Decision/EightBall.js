1e1f45d87a1b1dd7ec7b3a7d36d7c8b268d4e590
James Johnson <music1384@yahoo.co.uk>
2017-03-23 22:17:22 +0000

    Added 8ball command to the bot.

--- Commands/Decision/EightBall.js
+++ Commands/Decision/EightBall.js
@@ -14,7 +14,39 @@ class EightBallCommand extends commando.Command
 
     async run (message, args)
     {
-        await message.channel.sendMessage ("Eight ball here!");
+        // The random phrases the bot can return.
+        var phrases = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes, definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."
+        ];
+
+        // Generate the index of the response and then store it.
+        var index = Math.floor (Math.random() * phrases.length) + 0;
+        var response = phrases[index];
+
+        // Remove the command from the question.
+        var question = message.content.replace ("~8ball", "");
+
+        // Send the response to the user.
+        await message.channel.startTyping ();
+        await message.channel.sendMessage (":question: **Question:** " + question + "\n:8ball: **Response:** " + response);
+        await message.channe.stopTyping (true);
     }
 }
 

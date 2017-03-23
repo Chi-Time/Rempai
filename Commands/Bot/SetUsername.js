16e6340136826a2f7c3d9e7080d7ba29624a3317
James Johnson <music1384@yahoo.co.uk>
2017-03-23 06:00:44 +0000

    Added documentation of code.

--- Commands/Bot/SetUsername.js
+++ Commands/Bot/SetUsername.js
@@ -14,21 +14,28 @@ class SetUsernameCommand extends commando.Command
 
     async run (message, args) 
     {
+        // Ensure the author is the owner of the bot.
         if(message.author.id == "182822432372031488")
         {
+            // Remove the command usage from the statement.
             var username = message.content.replace ("~setusername ", "");
-        
+
+            // Change the bot's username.
             await message.client.user.setUsername (username);
 
+            // Display message to user informing name change.
             await message.channel.startTyping ();
             await message.channel.sendMessage ("My name has been successfully changed! ^-^ I hope you like it boo.");
             await message.channel.stopTyping (true);
 
+            // Log the name change into the console.
             console.log ("Username was changed!\nNew username is: " + username);
 
+            // Break from the function.
             return;
         }
 
+        // Display message to the user informing of incorrect usage.
         await message.channel.startTyping ();
         await message.channel.sendMessage ("Only bot owners are allowed to use this command!!");
         await message.channel.stopTyping (true);

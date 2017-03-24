7432879b9262ff4f105b580b5d269648c9801398
James Johnson <music1384@yahoo.co.uk>
2017-03-24 10:18:07 +0000

    Added permission check to channel topic.

--- Commands/Admin/SetChannelTopic.js
+++ Commands/Admin/SetChannelTopic.js
@@ -14,17 +14,25 @@ class SetChannelTopicCommand extends commando.Command
 
     async run (message, args)
     {
-        await message.channel.startTyping ();
+        // Ensure that the user has the permission to change the channel topic.
+        if(message.member.permissions.hasPermission ("MANAGE_CHANNELS"))
+        {
+            await message.channel.startTyping ();
 
-        // Remove the command from the message and returns the topic.
-        var topic = message.content.replace ("~topic ", "");
+            //Remove the command from the message and returns the topic.
+            var topic = message.content.replace ("~topic ", "");
 
-        // Change the channel topic to the topic provided.
-        message.channel.setTopic (topic).catch (console.error);
+            // Change the channel topic to the topic provided.
+            message.channel.setTopic (topic).catch (console.error);
 
-        // Inform user of channel topic change.
-        await message.channel.sendMessage ("Channel topic has been changed! ^.^ Happy to help.");
-        await message.channe.stopTyping (true);
+            // Inform user of channel topic change.
+            await message.channel.sendMessage ("Channel topic has been changed! ^.^ Happy to help.");
+            await message.channel.stopTyping (true);
+        }
+
+        // Inform the user as to why it failed.
+        await message.channel.sendMessage ("I can't do that!\nChannels can only be _alphanumeric_ with dashes: '-' and underscores: '_'\nSorry!");
+        await message.channel.stopTyping (true);
     }
 }
 

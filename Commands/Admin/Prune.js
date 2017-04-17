4250c879d8191a60288089954d4626945e82d222
James Johnson <music1384@yahoo.co.uk>
2017-04-17 06:16:54 +0100

    Added debug functionality for message deletion.

--- Commands/Admin/Prune.js
+++ Commands/Admin/Prune.js
@@ -18,11 +18,25 @@ class PruneCommand extends commando.Command
 
         var ma = m.array();
 
+        DebugMessages(messages);
+
         // Send the response to the user.
         await message.channel.startTyping ();
         await message.channel.sendMessage ("hey");
         await message.channel.stopTyping (true);
     }
+}
+
+function DebugMessages (messages)
+{
+    var now = new Date();
+    console.log(now.toLocaleString() + ": " + messages.length + " Messages were pruned.");
+    console.log("These are the following messages that were pruned:\n");
+
+    for(let i = 0; i < messages.length; i++)
+        console.log(i + " - " + messages[i].content);
+    
+    console.log("");
 }
 
 module.exports = PruneCommand;
